6ac654bb685e6e64744b3cd59c942b7b7b1ac3fe
Masaaki Matsunaga <castle2164m@gmail.com>
2017-02-05 19:51:53 +0900

    programing2-report7-firstcommit pseudo-codeの作成

---
+++ src/main/java/jp/ac/uryukyu/ie/e145165/Report7.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e145165;
+
+/**
+ * Created by masaaki on 2017/02/05.
+ */
+public class Report7 {
+    public static void main(String args[]){
+
+        //答えの配列を用意
+        //プレイヤーが入力した数字を格納する配列を用意
+
+        //C言語だとプレイヤーの入力はscanfを使いたいが、javaだと何を使うのか確認
+
+        /* プレイヤーが入力した数値が答えと照らし合わせて
+         * 桁と数字があっているのかどうかをhit(桁と数字があっている)とball(答えに同じ数字がある)を用いて表示
+         */
+
+        //プレイヤーの入力がhit==3になればgameclearとする
+
+
+    }
+}
