b281bb1ef326a98a41bf25d5152e47cca0f74c80
Masaaki Matsunaga <castle2164m@gmail.com>
2017-02-06 20:48:15 +0900

    programing2-report7-final_commit 課題の条件に従い、newReport7クラスのmakeAnswerメソッドをユニットテストするためのnewReport7Testを作成

---
+++ src/test/java/jp/ac/uryukyu/ie/e145165/newReport7Test.java
@@ -0,0 +1,17 @@
+package jp.ac.uryukyu.ie.e145165;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by masaaki on 2017/02/06.
+ */
+public class newReport7Test {
+    @Test
+    public void makeAnswer() throws Exception {
+        newReport7 test = new newReport7();
+        assertNotNull(test);
+    }
+
+}
